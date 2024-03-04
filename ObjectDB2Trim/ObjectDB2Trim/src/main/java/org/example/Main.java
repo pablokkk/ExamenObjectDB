4ace1c61499c8e6f721964a1c0af207d7482dfc2
pablokkk <pablorobleslorenzo@gmail.com>
2024-03-04 14:05:28 +0100

    Ejercicio 1

--- ObjectDB2Trim/ObjectDB2Trim/src/main/java/org/example/Main.java
+++ ObjectDB2Trim/ObjectDB2Trim/src/main/java/org/example/Main.java
@@ -8,11 +8,11 @@ public class Main {
     public static void main(String[] args) {
         clienteController clienteController = new clienteController();
 
-        var cliente1 = new cliente(1L, "Francisco", 10L, true );
-        var cliente2 = new cliente(2L, "Adrian", 5L, true );
-        var cliente3 = new cliente(3L, "Pablo", 8L, false );
-        var cliente4 = new cliente(4L, "Alejandro", 2L, false );
-        var cliente5 = new cliente(5L, "Samuel", 12L, true );
+        new cliente(1L, "Francisco", 10L, true);
+        new cliente(2L, "Adrian", 5L, true);
+        new cliente(3L, "Pablo", 8L, false);
+        new cliente(4L, "Alejandro", 2L, false);
+        new cliente(5L, "Samuel", 12L, true);
 
         clienteController.getCliente(4L);
         clienteController.listarMejoresClientes(11L);
